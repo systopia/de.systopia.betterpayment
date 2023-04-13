bd2660bffba3d56dd13c54ba0238f0166ebcb9ad
Jo Franz <franz@systopia.de>
2023-04-13 13:44:57 +0200

    civix upgrade 23.02.1

--- betterpayment.php
+++ betterpayment.php
@@ -24,32 +24,12 @@ function betterpayment_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @param array $files
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function betterpayment_civicrm_xmlMenu(&$files) {
-  _betterpayment_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
  */
 function betterpayment_civicrm_install() {
   _betterpayment_civix_civicrm_install();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function betterpayment_civicrm_uninstall() {
-  _betterpayment_civix_civicrm_uninstall();
 }
 
 /**
@@ -61,82 +41,6 @@ function betterpayment_civicrm_enable() {
   _betterpayment_civix_civicrm_enable();
   betterpayment_pp_install();
 }
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function betterpayment_civicrm_disable() {
-  _betterpayment_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed
- *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function betterpayment_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _betterpayment_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function betterpayment_civicrm_managed(&$entities) {
-  _betterpayment_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * @param array $caseTypes
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function betterpayment_civicrm_caseTypes(&$caseTypes) {
-  _betterpayment_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function betterpayment_civicrm_angularModules(&$angularModules) {
-_betterpayment_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function betterpayment_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _betterpayment_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
 
 /**
  * Will register the payment processor
